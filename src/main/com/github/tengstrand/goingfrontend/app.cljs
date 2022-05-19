7264d58df5ec8dc21420e897e67b7702526add01
Joakim Tengstrand <joakimtengstrand@gmail.com>
2022-05-19 16:15:33 +0200

    A first web app using Reagent

--- src/main/com/github/tengstrand/goingfrontend/app.cljs
+++ src/main/com/github/tengstrand/goingfrontend/app.cljs
@@ -1,4 +1,12 @@
-(ns com.github.tengstrand.goingfrontend.app)
+(ns com.github.tengstrand.goingfrontend.app
+  (:require [reagent.dom :as r]))
 
-(defn init []
-  (println "Hello!!"))
+(defn main-component []
+  [:div
+   [:h1 "This is a simple web app!"]])
+
+(defn mount [c]
+  (r/render [c] (.getElementById js/document "app")))
+
+(defn main []
+  (mount main-component))
